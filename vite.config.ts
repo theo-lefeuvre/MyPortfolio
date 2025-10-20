bab481ef392a85e5e20431916a8a75711559a340
Théo Lefeuvre <theo.lefeuvre@hotmail.fr>
2025-10-20 14:32:30 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Replace with your actual repository name
+  base: '/MyPortfolio/', // Replace with your actual repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
